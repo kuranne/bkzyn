4c4ee19986bc1a7285b9521814576c6f8e46875b
Kuramais <kurannemaisa@outlook.com>
2026-07-09 12:05:41 +0700

    delete: test scract files

--- test_toml.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let mut doc = "[config]\n".parse::<toml_edit::DocumentMut>().unwrap();
-    doc["config.nvim"] = toml_edit::Item::Table(toml_edit::Table::new());
-    println!("{}", doc.to_string());
-}
